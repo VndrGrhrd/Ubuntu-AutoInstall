76e7c140ee94200e462beee311e5e45679cefeb0
Vandoir Gerhard <80967598+VndrGrhrd@users.noreply.github.com>
2025-08-20 14:31:00 -0300

    Update setup-neural-amp-modeler.sh
    remove sudo

--- setup-neural-amp-modeler.sh
+++ setup-neural-amp-modeler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # cria pasta LV2
 mkdir -p /home/vandoir/.lv2
-sudo chown -R vandoir:vandoir /home/vandoir/.lv2
+chown -R vandoir:vandoir /home/vandoir/.lv2
 
 # baixa Ratatouille
 wget -O /tmp/ratatouille.lv2.tar.xz https://github.com/brummer10/Ratatouille.lv2/releases/latest/download/Ratatouille.lv2-v0.9.11-linux-x86_64.tar.xz
@@ -9,7 +9,7 @@ wget -O /tmp/ratatouille.app.tar.xz https://github.com/brummer10/Ratatouille.lv2
 sudo wget -O /usr/share/icons/nam_icon_256x256.png https://raw.githubusercontent.com/sdatkinson/NeuralAmpModelerPlugin/refs/heads/main/NeuralAmpModeler/resources/Images.xcassets/AppIcon.appiconset/icon_256x256.png
 # extrai
 tar -xJf /tmp/ratatouille.lv2.tar.xz -C /home/vandoir/.lv2
-sudo tar -xJf /tmp/ratatouille.app.tar.xz -C /opt
+tar -xJf /tmp/ratatouille.app.tar.xz -C /opt
 
 # cria desktop
 cat >/home/vandoir/.local/share/applications/ratatouille.desktop <<'EOF'
