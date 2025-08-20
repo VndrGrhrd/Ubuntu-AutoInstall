baf13958d361f53002a78af44e8205117805b4cc
Vandoir Gerhard <80967598+VndrGrhrd@users.noreply.github.com>
2025-08-20 13:47:49 -0300

    Update setup-neural-amp-modeler.sh
    fix permissions

--- setup-neural-amp-modeler.sh
+++ setup-neural-amp-modeler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # cria pasta LV2
 mkdir -p /home/vandoir/.lv2
-chown -R vandoir:vandoir /home/vandoir/.lv2
+sudo chown -R vandoir:vandoir /home/vandoir/.lv2
 
 # baixa Ratatouille
 wget -O /tmp/ratatouille.lv2.tar.xz https://github.com/brummer10/Ratatouille.lv2/releases/latest/download/Ratatouille.lv2-v0.9.11-linux-x86_64.tar.xz
@@ -9,17 +9,15 @@ wget -O /tmp/ratatouille.app.tar.xz https://github.com/brummer10/Ratatouille.lv2
 
 # extrai
 tar -xJf /tmp/ratatouille.lv2.tar.xz -C /home/vandoir/.lv2
-mkdir -p /opt
-tar -xJf /tmp/ratatouille.app.tar.xz -C /opt
-chown -R vandoir:vandoir /home/vandoir/.lv2
+sudo tar -xJf /tmp/ratatouille.app.tar.xz -C /opt
 
 # cria desktop
-cat >/usr/share/applications/ratatouille.desktop <<'EOF'
+cat >/home/vandoir/.local/share/applications/ratatouille.desktop <<'EOF'
 [Desktop Entry]
 Version=1.0
 Name=Ratatouille
-Comment=LV2 Audio Plugin Host
-Exec=/opt/Ratatouille*/Ratatouille*
+Comment=NAM (Neural AMP Modeler) LV2 Audio Plugin Host
+Exec=/opt/Ratatouille.lv2-app-v0.9.11/Ratatouille
 Icon=audio-card
 Terminal=false
 Type=Application
